c246d14c0d174e126e8b3a5a7120f03a877fe5af
clrnd <welcometothechango@gmail.com>
2020-04-28 17:50:21 -0300

    Minimum working example

---
+++ counter.js
@@ -0,0 +1,18 @@
+const { t, ngettext, msgid, addLocale, useLocale } = require('ttag');
+
+const locale = process.env.LOCALE;
+
+if (locale) {
+    const translationObj = require(`./${locale}.po.json`);
+    addLocale(locale, translationObj);
+    useLocale(locale);
+}
+
+function startCount(n) {
+    console.log(t`starting count up to ${n}`);
+    for (let i = 0; i <= n; i++) {
+        console.log(ngettext(msgid`one tick passed`, `${i} ticks passed`, i));
+    }
+}
+
+startCount(3);
